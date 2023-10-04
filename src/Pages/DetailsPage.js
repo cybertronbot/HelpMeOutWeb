e9a4a7527b64e5f9606bd87edf878c73f3b8a77d
Okhuomon Ajayi <okhuomonajayi54@gmail.com>
2023-10-04 23:58:26 +0100

    commit

--- src/Pages/DetailsPage.js
+++ src/Pages/DetailsPage.js
@@ -8,6 +8,7 @@ import Telegram from "../assets/telegram.svg";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Button } from "../components/button/button";
+import  video1  from "../assets/video1.png";
 
 const DetailsPage = () => {
   return (
@@ -15,69 +16,75 @@ const DetailsPage = () => {
       <div clsssName="">
         <Navbar />
 
-        <section className="flex px-16 mt-10">
-          <div className="flex flex-col  w-[700px]">
-            <div>
-              <h1 className="font-bold text-[40px] text-secondary font-sora">
-                Your video is ready!
-              </h1>
-            </div>
-            <p className="font-sora text-[#727272] mt-4">Name</p>
-            <div className="flex gap-6 items-center">
-              <p className="font-semibold text-[#413C6D] font-sora text-[18px]">
-                Untitled_Video_20232509
-              </p>
-              <img src={Edit} alt="edit-icon" />
-            </div>
-
-            <div className="rounded-[16px] flex items-center bg-[#dcdcdf] h-[60px] w-[500px] px-3 mt-14">
-              <input
-                type="text"
-                className="w-full h-full p-2 outline-none bg-[#b6b3c620] text-sm placeholder-black placeholder:text-sm font-sora"
-                placeholder="Enter email of receiver"
-              />
-              <button className=" bg-[#605C84] px-[16px] py-[8px] text-white rounded-[8px] cursor-pointer font-sora">
-                Send
-              </button>
-            </div>
-
-            <div className="mt-10">
-              <p className="text-sora text-secondary mb-2 font-semibold">
-                Video Url
-              </p>
-              <div className="rounded-[16px] flex justify-between items-center border border-[#929292] h-[60px] w-[500px] px-3  ">
-                <p className="text-sm font-sora">
-                  https://www.helpmeout/Untitled_Video_20232509
+        <section className="grid grid-cols-7 gap-10 px-16 mt-10">
+          <div className="col-span-3">
+            <div className="flex flex-col  w-[700px]">
+              <div>
+                <h1 className="font-bold text-[40px] text-secondary font-sora">
+                  Your video is ready!
+                </h1>
+              </div>
+              <p className="font-sora text-[#727272] mt-4">Name</p>
+              <div className="flex gap-6 items-center">
+                <p className="font-semibold text-[#413C6D] font-sora text-[18px]">
+                  Untitled_Video_20232509
                 </p>
-                <button className="font-sora border border-[#120B48] px-[14px] py-[8px]  rounded-[8px] cursor-pointer text-sm flex gap-2">
-                  <img className="" src={Copy} alt="" />
-                  Copy
+                <img src={Edit} alt="edit-icon" />
+              </div>
+
+              <div className="rounded-[16px] flex items-center bg-[#dcdcdf] h-[60px] w-[500px] px-3 mt-14">
+                <input
+                  type="text"
+                  className="w-full h-full p-2 outline-none bg-[#b6b3c620] text-sm placeholder-black placeholder:text-sm font-sora"
+                  placeholder="Enter email of receiver"
+                />
+                <button className=" bg-[#605C84] px-[16px] py-[8px] text-white rounded-[8px] cursor-pointer font-sora">
+                  Send
                 </button>
               </div>
-            </div>
 
-            <div className="mt-12 text-[14px] font-semibold">
-              <p className="mb-2">Share your video</p>
-              <div className="flex items-center gap-4 font-inter">
-                <button className="rounded-lg border-2 px-4 flex py-[8px] gap-1">
-                  <img src={Facebook} alt="" />
-                  Facebook
-                </button>
-                <button className="rounded-lg border-2 px-4 flex py-[8px] gap-1">
-                  <img src={Whatsapp} alt="" />
-                  Whatsapp
-                </button>
-                <button className="rounded-lg border-2 px-4 flex py-[8px]  gap-1">
-                  <img src={Telegram} alt="" />
-                  Telegram
-                </button>
+              <div className="mt-10">
+                <p className="text-sora text-secondary mb-2 font-semibold">
+                  Video Url
+                </p>
+                <div className="rounded-[16px] flex justify-between items-center border border-[#929292] h-[60px] w-[500px] px-3  ">
+                  <p className="text-sm font-sora">
+                    https://www.helpmeout/Untitled_Video_20232509
+                  </p>
+                  <button className="font-sora border border-[#120B48] px-[14px] py-[8px]  rounded-[8px] cursor-pointer text-sm flex gap-2">
+                    <img className="" src={Copy} alt="" />
+                    Copy
+                  </button>
+                </div>
+              </div>
+
+              <div className="mt-12 text-[14px] font-semibold">
+                <p className="mb-2">Share your video</p>
+                <div className="flex items-center gap-4 font-inter">
+                  <button className="rounded-lg border-2 px-4 flex py-[8px] gap-1">
+                    <img src={Facebook} alt="" />
+                    Facebook
+                  </button>
+                  <button className="rounded-lg border-2 px-4 flex py-[8px] gap-1">
+                    <img src={Whatsapp} alt="" />
+                    Whatsapp
+                  </button>
+                  <button className="rounded-lg border-2 px-4 flex py-[8px]  gap-1">
+                    <img src={Telegram} alt="" />
+                    Telegram
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-         
-          <div>Video</div>
+
+          <div className="col-span-4 ">
+            <div >
+              <img className="rounded-[8px]" src={video1} />
+            </div>
+          </div>
         </section>
-        <div className="flex justify-center items-center flex-col gap-7 mt-7">
+        <div className="flex justify-center items-center flex-col gap-7 mt-32">
           <h4 className="text-center text-secondary font-sora text-[20px] font-semibold">
             To ensure the availability and privacy of your video,
             <br />
